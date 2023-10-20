6e9854eb73df622a3bca8f144739d71c86124df0
Enrique <e.holguin993@gmail.com>
2023-10-19 19:15:36 -0500

    Button and Test components deleted

--- app/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Button() {
-    return (
-        <>
-            <button className='bg-primary text-white rounded-xl'>Prueba2</button>
-        </>
-    )
-}
--- app/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Test() {
-    return (
-        <div>Test1</div>
-    )
-}
-
-export default Test
