9601b2a11f435b400e90e27d668dfe13668edce3
Enrique <e.holguin993@gmail.com>
2024-01-10 18:13:59 -0500

    refactor Carousel component

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -1,43 +1,10 @@
-'use client'
-
-import Slider, { Settings } from "react-slick";
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import Slide from "./Slider";
 
 const Carousel = () => {
-  const settings:Settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    cssEase: "linear"
-  };
   return (
-    <div id="Carousel">
-      <h2>Auto Play</h2>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
+    <div className="mt-5" id="Carousel">
+      <h2>Auto play</h2>
+      <Slide></Slide>
     </div>
   );
 }
