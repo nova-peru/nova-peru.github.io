44b2f33ff0d9e1bb9aeb010ddf99fba807c46ef8
Enrique <e.holguin993@gmail.com>
2024-01-10 18:13:23 -0500

    add Slide Component

---
+++ components/Carousel/Slider.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import Slider, { Settings } from "react-slick";
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+const Slide = () => {
+    const settings:Settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear"
+      };
+  return (
+      <Slider className="mt-5" {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+  )
+}
+
+export default Slide
