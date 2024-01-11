2ec205b4f0a77356985e21aeb9ec2b8fb17d7a36
Enrique <e.holguin993@gmail.com>
2024-01-10 20:42:04 -0500

    add array of icons

---
+++ components/Carousel/icons.ts
@@ -0,0 +1,11 @@
+import aws from './icons/aws.svg'
+import nodejs from './icons/nodejs.svg'
+import react from './icons/react.svg'
+import salesforce from './icons/salesforce.svg'
+import postgresql from './icons/postgresql-plain-wordmark.svg'
+import typescript from './icons/typescript-icon.svg'
+import mongodb from './icons/mongodb-original-wordmark.svg'
+import php from './icons/php.svg'
+import redis from './icons/redis-original-wordmark.svg'
+
+export const icons:any[] = [aws,php,nodejs,react,salesforce,typescript,postgresql,mongodb,redis]
