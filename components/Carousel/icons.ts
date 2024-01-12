6ea2af708a1c27d795e27d247078e0490104f108
Enrique <e.holguin993@gmail.com>
2024-01-11 20:28:22 -0500

    add react-native to icons

--- components/Carousel/icons.ts
+++ components/Carousel/icons.ts
@@ -7,5 +7,6 @@ import typescript from './icons/typescript-icon.svg'
 import mongodb from './icons/mongodb-original-wordmark.svg'
 import php from './icons/php.svg'
 import redis from './icons/redis-original-wordmark.svg'
+import reactnative from './icons/sdk-react-native.svg'
 
-export const icons:any[] = [aws,php,nodejs,react,salesforce,typescript,postgresql,mongodb,redis]
+export const icons:any[] = [aws,php,nodejs,react,salesforce,typescript,postgresql,mongodb,redis,reactnative]
