c6e953e0ea61c11e96547babd25ee0f818da6b8c
Enrique <e.holguin993@gmail.com>
2024-01-17 16:50:58 -0500

    add send route

---
+++ app/api/send/route.tsx
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL as string
+const toEmail = process.env.TO_EMAIL as string
+
+
+export async function POST(req: Request) {
+  const { email, message, name } = await req.json();
+  const subject = `${name} want contact you`
+  try {
+    const { data } = await resend.emails.send({
+      from: fromEmail,
+      to: [toEmail, email],
+      subject: subject,
+      react: (
+        <>
+          <h1>{`My name is ${name}`}</h1>
+          <p>Thank you for contacting me!</p>
+          <p>Message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
