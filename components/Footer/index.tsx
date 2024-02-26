3dd9d25fce0db9324a205a045d887b4771beac0d
Enrique <e.holguin993@gmail.com>
2024-02-25 21:16:53 -0500

    feat: add nova-logo header & footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image"
 
 const Footer = () => {
   return (
@@ -10,17 +11,17 @@ const Footer = () => {
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12">
-              <div className="mb-12 max-w-[360px] lg:mb-16">
+              <div className="mb-12 max-w-[240px] lg:mb-16">
                 <Link href="/" className="mb-8 inline-block">
                   <img
-                    src="images/logo/logo-2.svg"
+                    src="images/logo/logo-nova-2.svg"
                     alt="logo"
                     className="w-full dark:hidden"
                     width={140}
                     height={30}
                   />
                   <img
-                    src="images/logo/logo.svg"
+                    src="images/logo/logo-nova.svg"
                     alt="logo"
                     className="hidden w-full dark:block"
                     width={140}
