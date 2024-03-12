4ecf46837315f722173b5a2befb75b0e835c3e61
enrique-holguin <116053539+enrique-holguin@users.noreply.github.com>
2024-03-12 12:45:17 -0500

    feat: update favicon & title (#22)
    * feat: update icon & title
    
    * feat: fix imports & width services images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import Head from "./head";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
+
+import { Providers } from "./providers";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+
 
 export default function RootLayout({
   children,
@@ -19,7 +23,7 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <Head />
 
       <body className="dark:bg-black">
         <Providers>
@@ -34,4 +38,4 @@ export default function RootLayout({
   );
 }
 
-import { Providers } from "./providers";
+
