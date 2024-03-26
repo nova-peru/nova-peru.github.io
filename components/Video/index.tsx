1fd8989833dff0d195bbfdeeabff25d74bfa958e
Enrique <e.holguin993@gmail.com>
2024-03-26 12:25:48 -0500

    feat: add VideoItem & videos

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,17 +1,13 @@
-"use client";
+'use client'
 
-import { useState } from "react";
 import SectionTitle from "../Common/SectionTitle";
-
-import ModalVideo from "react-modal-video";
+import VideoItem from "./VideoItem";
+import { videos } from './videos';
 
 const Video = () => {
-  const [isOpen, setOpen] = useState(false);
-
   return (
     <section className="relative z-10 py-16 md:py-20 lg:py-28">
       <div className="container">
-        {/* <div>Chatbot:</div> */}
         <SectionTitle
           title="Chatbot:"
           subtitle="Descubre el potencial de la IA en tu negocio"
@@ -20,47 +16,10 @@ const Video = () => {
           center
           mb="80px"
         />
-
-        <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4">
-            <div
-              className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
-              data-wow-delay=".15s"
-            >
-              <div className="relative aspect-[77/40] items-center justify-center">
-                <img data-cy="video-image" src="images/video/video.jpg" alt="video image" />
-                <div className="absolute top-0 right-0 flex h-full w-full items-center justify-center">
-                  <button
-                    disabled
-                    onClick={() => setOpen(true)}
-                    className="hidden"
-                  // className="flex h-[70px] w-[70px] items-center justify-center rounded-full bg-white bg-opacity-75 text-primary transition hover:bg-opacity-100"
-                  >
-                    <svg
-                      width="16"
-                      height="18"
-                      viewBox="0 0 16 18"
-                      className="fill-current"
-                    >
-                      <path d="M15.5 8.13397C16.1667 8.51888 16.1667 9.48112 15.5 9.86602L2 17.6603C1.33333 18.0452 0.499999 17.564 0.499999 16.7942L0.5 1.20577C0.5 0.43597 1.33333 -0.0451549 2 0.339745L15.5 8.13397Z" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="flex flex-row flex-nowrap overflow-x-auto">
+          {videos.map(video => <VideoItem key={video.id} id={video.id} image={video.image}></VideoItem>)}
         </div>
       </div>
-
-      <ModalVideo
-        channel="youtube"
-        autoplay={true}
-        start={true}
-        isOpen={isOpen}
-        videoId="L61p2uyiMSo"
-        onClose={() => setOpen(false)}
-      />
-
       <div className="absolute bottom-0 left-0 right-0 z-[-1]">
         <img src="images/video/shape.svg" alt="shape" className="w-full" />
       </div>
---
+++ components/Video/videos.ts
@@ -0,0 +1,19 @@
+type Video = {
+  id: string,
+  image: string
+}
+
+export const videos : Video[] = [
+  {
+    id: 'tp0Ycr_NnMI',
+    image: 'images/video/bot-1.jpg'
+  },
+  {
+    id: 'P7pmkgtqhHg',
+    image: 'images/video/video.jpg'
+  },
+  {
+    id: 'VGH9NL7pDOQ',
+    image: 'images/video/video.jpg'
+  }
+]
